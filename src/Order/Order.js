f0678361f9cfd5ff68dd5e0363f9da2e20c446ec
kobylan <kobylan.kaz@gmail.com>
2020-05-16 19:03:07 +0600

    asd

---
+++ src/Order/Order.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Tags from "./Tags";
+import TextArea from "./TextArea";
+import styled from "styled-components";
+
+const Form = styled.div`
+  border: 1px solid #efbb35;
+  width: 50%;
+  margin: 0 auto;
+  box-shadow: 0px 4px 10px -8px black;
+  border-radius: 3px;
+`;
+
+class Order extends Component {
+  render() {
+    return (
+      <Form>
+        <TextArea />
+        <Tags />
+      </Form>
+    );
+  }
+}
+
+export default Order;
